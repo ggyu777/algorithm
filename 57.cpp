05de4ba06e63f72273b12669fe5d0b2fa3f2a2e0
ggyu777 <73706252+ggyu777@users.noreply.github.com>
2021-02-07 22:31:53 +0900

    Create 57.cpp

---
+++ 57.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+
+void Leejinsu(int value) {
+	if (value==1)
+	{
+		printf("%d", value);
+		return;
+	}
+	else
+	{
+		Leejinsu(value / 2);
+		printf("%d", value%2);
+	}
+}
+
+int main() {
+	int N;
+	scanf("%d", &N);
+	Leejinsu(N);
+}
