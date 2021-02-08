3877ce85a930acaa5e07321ea1d40e6f4022fdc8
ggyu777 <73706252+ggyu777@users.noreply.github.com>
2021-02-08 22:31:52 +0900

    Create 59.cpp

---
+++ 59.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int ch[11];
+int n;
+void D(int v) {
+	int i;
+	if (v == n + 1) {
+		for (i = 1; i <= n; i++) {
+			if (ch[i] == 1) printf("%d ", i);
+		}
+		printf("\n");
+	}
+	else {
+		ch[v] = 1;
+		D(v + 1);
+		ch[v] = 0;
+		D(v + 1);
+	}
+}
+
+int main() {
+	scanf("%d", &n);
+
+		D(1);
+	
+	return 0;
+}
