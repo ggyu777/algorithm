88822ffed83ef0776d4e6a7f4a27d1e316f930fb
ggyu777 <73706252+ggyu777@users.noreply.github.com>
2021-02-09 23:06:45 +0900

    Create 64.cpp

---
+++ 64.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+void DFS(int v) {
+	if (v == n) {
+
+	}
+}
+
+
+int main() {
+	int n, m, i, a, b,cnt=0;
+	int array[21][21] = { 0 };
+	scanf("%d %d", &n, &m);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d %d", &a, &b);
+		array[a][b] =1 ;
+	}
+
+	DFS(1);
+}
