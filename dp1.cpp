b45636158b982f33e62cc351228e1020a796fd2a
ggyu777 <73706252+ggyu777@users.noreply.github.com>
2021-03-08 21:17:08 +0900

    Create dp1.cpp

---
+++ dp1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int a;
+	int X[46];
+	X[1] = 1;
+	X[2] = 2;
+	for (int i = 3; i < 46; i++)
+	{
+		X[i] = X[i - 2] + X[i - 1];
+	}
+
+	cin >> a;
+	cout << X[a];
+}
