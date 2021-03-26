ff49aa00169c9a17dfa2f08a640683a21d8f1118
ggyu777 <73706252+ggyu777@users.noreply.github.com>
2021-03-26 22:05:55 +0900

    Create dp2.cpp

---
+++ dp2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int dx[101];
+int D(int N) {
+	if (dx[N] > 0) return dx[N];
+	if (N == 1 || N == 2) return N;
+	else return dx[N] = D(N - 1) + D(N - 2);
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int a;
+	cin >> a;
+	cout << D(a);
+
+
+
+}
